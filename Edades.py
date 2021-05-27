e483435dd8337b591e8f35a11e3c9fab11f72ba2
lagunes269 <lagunes1098@gmail.com>
2021-05-26 21:03:26 -0500

    Programa que recibe nombre y edad y te dice quien es mas grande

---
+++ Edades.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+nombre_1 = input('Introduce un nombre: ')
+
+
+# In[18]:
+
+
+edad_1= int (input('Indroduce edad: '))
+
+
+# In[19]:
+
+
+nombre_2 = input('Introduce un nombre: ')
+
+
+# In[20]:
+
+
+edad_2= int (input('Indroduce edad: '))
+
+
+# In[21]:
+
+
+if edad_1 > edad_2:
+    print (nombre_1)
+else:
+    print (nombre_2)
+
